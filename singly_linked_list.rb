e4fdcd1acf2c1aa16ec38ae3e402077c34fabace
Ryan Sobol <contact@ryansobol.com>
2013-08-28 20:52:19 -0700

    Create singly_linked_list.rb

---
+++ singly_linked_list.rb
@@ -0,0 +1,47 @@
+class SinglyLinkedList
+  class Node
+    def initialize(item, nexxt)
+      @item = item
+      @next = nexxt
+    end
+
+    attr_accessor :item, :next
+
+    def clear
+      temp = item
+      self.item = self.next = nil
+      temp
+    end
+  end
+
+  def initialize
+    @size = 0
+  end
+
+  attr_reader :size
+
+  def shift(element)
+    @size += 1
+    @head = Node.new(element, @head)
+    element
+  end
+
+  def empty?
+    @head.nil?
+  end
+
+  def first
+    empty? ? nil : @head.item
+  end
+
+  def unshift
+    return nil if empty?
+
+    @size -= 1
+
+    temp  = @head
+    @head = temp.next
+
+    temp.clear
+  end
+end
